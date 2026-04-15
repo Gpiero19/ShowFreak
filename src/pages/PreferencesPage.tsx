0068a133b3ad5ff43956640813eb4c1edc46416c
Gian Piero Canevari <canevarigian@gmail.com>
2026-04-15 19:21:13 +0200

    chore: clean worktree - remove backend files from frontend

--- src/pages/PreferencesPage.tsx
+++
@@ -1,8 +0,0 @@
-export default function PreferencesPage() {
-  return (
-    <div className="preferences-page">
-      <h1>Preferences</h1>
-      <p>Manage your content preferences</p>
-    </div>
-  )
-}
